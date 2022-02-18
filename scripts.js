7b2c9eab2435a4121739837b2149b14337ac61bf
Nikith Kalu <nikithkalu@gmail.com>
2022-02-18 21:38:43 +1100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 const countEl = document.getElementById('count');
 
+updateVisitCount();
+
 function updateVisitCount() {
-    fetch('https://api.countapi.xyz/update/nikithkalu/portfolio/?amount=1')
-        .then(res => res.json())
-        .then(res => {
-            countEl.innerHTML = res.value;
-        });
+	fetch('https://api.countapi.xyz/update/nikithkalu/portfolio/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
 }
