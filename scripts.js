f262c4ee5e5918020393789f68e7b269cbd0b59e
Nikith Kalu <nikithkalu@gmail.com>
2022-02-19 12:34:44 +1100

    view count fix

--- scripts.js
+++ scripts.js
@@ -1,11 +1,3 @@
-const countEl = document.getElementById('count');
-
-updateVisitCount();
-
-function updateVisitCount() {
-	fetch('https://api.countapi.xyz/update/nikithkalu/portfolio/?amount=1')
-	.then(res => res.json())
-	.then(res => {
-		countEl.innerHTML = res.value;
-	})
+function websiteVisits(response) {
+	document.querySelector("#visits").textContent = response.value;
 }
